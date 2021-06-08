24c151a1431926bec2aded99628f68d97e708dbf
Miroslav <psapunar@fi.uba.ar>
2021-06-07 21:42:53 -0300

    carreras de especializacion

--- lib/fiuba_web_migration.ex
+++ lib/fiuba_web_migration.ex
@@ -1,20 +1,18 @@
 defmodule FiubaWebMigration do
-
   import Migracion_noticias
   import Migracion_carrera_grado
+  import Migracion_carrera_especializacion
   import Migracion_maestria
   import Migracion_anuales_bianuales
   import Migracion_investigacion
 
   def migration() do
+    # carreras_grado()
+    # maestrias_posgrado()
+    carreras_especializaciones()
+    # anuales_bianuales()
+    # aca falta carreras de especializacion
 
-    #carreras_grado()
-    #maestrias_posgrado()
-    #anuales_bianuales()
-    #aca falta carreras de especializacion
-
-    #investigacion()
-
+    # investigacion()
   end
-
 end
