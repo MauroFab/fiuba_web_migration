c714c968b6188b783528a40b3b16ed31a28d4fd1
Miroslav <psapunar@fi.uba.ar>
2021-06-10 01:51:37 -0300

    migracion docentes

---
+++ lib/fiuba_web_migration/migracion_docentes.ex
@@ -0,0 +1,44 @@
+defmodule Migracion_docentes do
+  import Utils
+
+  alias FiubaWebMigration.Repo
+
+  def cargar_docentes() do
+    query_sql = "SELECT
+        menu_links.mlid AS mlid,
+        menu_links.link_title AS titulo,
+        REPLACE(menu_links.link_path, 'node/','') AS nid
+      FROM menu_links
+      WHERE menu_links.mlid = 906
+      AND menu_links.router_path= 'node/%';"
+
+    {:ok, respuesta} = Repo.query(query_sql)
+    respuesta.rows
+  end
+
+  def docentes() do
+    docentes = cargar_docentes() |> Enum.at(0)
+
+    nid = docentes |> Enum.at(2)
+    nodo = cargar_nodo(nid) |> Enum.at(0)
+
+    nombre_pagina = nodo |> Enum.at(0)
+    texto_pagina = nodo |> Enum.at(1)
+
+    jerarquia_padre = "Docentes"
+
+    id_pagina_docentes = crear_pagina(nombre_pagina, texto_pagina, jerarquia_padre)
+
+    url_docentes = "/docentes"
+    crear_navegacion(url_docentes, nombre_pagina, id_pagina_docentes)
+
+    docentes_opts = docentes |> Enum.at(0) |> cargar_hijos()
+
+    Enum.map(
+      docentes_opts,
+      fn elemento ->
+        busqueda_recursiva(elemento, url_docentes, nombre_pagina, jerarquia_padre)
+      end
+    )
+  end
+end
