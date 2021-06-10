44635aa610fa110d25f8af593f8670e61afa289a
Miroslav <psapunar@fi.uba.ar>
2021-06-10 01:52:07 -0300

    migracion extranjeros

---
+++ lib/fiuba_web_migration/migracion_extranjeros.ex
@@ -0,0 +1,44 @@
+defmodule Migracion_extranjeros do
+  import Utils
+
+  alias FiubaWebMigration.Repo
+
+  def cargar_extranjeros() do
+    query_sql = "SELECT
+        menu_links.mlid AS mlid,
+        menu_links.link_title AS titulo,
+        REPLACE(menu_links.link_path, 'node/','') AS nid
+      FROM menu_links
+      WHERE menu_links.mlid = 1602
+      AND menu_links.router_path= 'node/%';"
+
+    {:ok, respuesta} = Repo.query(query_sql)
+    respuesta.rows
+  end
+
+  def extranjeros() do
+    extranjeros = cargar_extranjeros() |> Enum.at(0)
+
+    nid = extranjeros |> Enum.at(2)
+    nodo = cargar_nodo(nid) |> Enum.at(0)
+
+    nombre_pagina = nodo |> Enum.at(0)
+    texto_pagina = nodo |> Enum.at(1)
+
+    jerarquia_padre = "Extranjeros"
+
+    id_pagina_extranjeros = crear_pagina(nombre_pagina, texto_pagina, jerarquia_padre)
+
+    url_extranjeros = "/extranjeros"
+    crear_navegacion(url_extranjeros, nombre_pagina, id_pagina_extranjeros)
+
+    extranjeros_opts = extranjeros |> Enum.at(0) |> cargar_hijos()
+
+    Enum.map(
+      extranjeros_opts,
+      fn elemento ->
+        busqueda_recursiva(elemento, url_extranjeros, nombre_pagina, jerarquia_padre)
+      end
+    )
+  end
+end
