cd42e4b8b81f013b6e83cce390bc659536bbc12b
Miroslav <psapunar@fi.uba.ar>
2021-06-15 19:51:30 -0300

    format utils

--- lib/fiuba_web_migration/utils.ex
+++ lib/fiuba_web_migration/utils.ex
@@ -15,7 +15,7 @@ defmodule Utils do
       WHERE menu_links.mlid = " <>
         to_string(nid) <>
         " AND menu_links.router_path= 'node/%';"
-        
+
     {:ok, respuesta} = Repo.query(query_sql)
     respuesta.rows
   end
@@ -34,32 +34,44 @@ defmodule Utils do
     respuesta.rows
   end
 
-
   # def id_pagina (url) do
   #   pagina_carrera_response =
   #     #         HTTPoison.get!("https://testing.cms.fiuba.lambdaclass.com/paginas?nombre=Carreras")
   # end
 
-  def cargar_imagen(url_imagen,nombre_imagen) do
-
-    {:ok, result} = HTTPoison.get(url_imagen) |> HTTPoison.Retry.autoretry(max_attempts: 10, wait: 20000, include_404s: false, retry_unknown_errors: false)
-
+  def cargar_imagen(url_imagen, nombre_imagen) do
+    {:ok, result} =
+      HTTPoison.get(url_imagen)
+      |> HTTPoison.Retry.autoretry(
+        max_attempts: 10,
+        wait: 20000,
+        include_404s: false,
+        retry_unknown_errors: false
+      )
 
     imagen = result.body
     headers = [{"Content-Type", "multipart/form-data"}]
     options = [ssl: [{:versions, [:"tlsv1.2"]}], recv_timeout: 20000]
 
     {:ok, response_imagen} =
-
-      (HTTPoison.request(
-      :post,
-      "https://testing.cms.fiuba.lambdaclass.com/upload",
-      {:multipart,
-       [{"file", imagen, {"form-data", [name: "files", filename: (nombre_imagen <> ".jpg")]}, [{"Content-Type", "image/jpeg"}]},{"type", "image/jpeg"}]},
-      headers,
-      options
-    )
-    |> HTTPoison.Retry.autoretry(max_attempts: 20, wait: 20000, include_404s: false, retry_unknown_errors: false))
+      HTTPoison.request(
+        :post,
+        "https://testing.cms.fiuba.lambdaclass.com/upload",
+        {:multipart,
+         [
+           {"file", imagen, {"form-data", [name: "files", filename: nombre_imagen <> ".jpg"]},
+            [{"Content-Type", "image/jpeg"}]},
+           {"type", "image/jpeg"}
+         ]},
+        headers,
+        options
+      )
+      |> HTTPoison.Retry.autoretry(
+        max_attempts: 20,
+        wait: 20000,
+        include_404s: false,
+        retry_unknown_errors: false
+      )
 
     response_body = response_imagen.body
     {:ok, response_body_map} = JSON.decode(response_body)
@@ -233,7 +245,12 @@ defmodule Utils do
   #   )
   # end
 
-  def crear_pagina(nombre_pagina \\ "", texto_pagina \\ "", jeraquia_pagina \\ "", id_menu_lateral \\ nil) do
+  def crear_pagina(
+        nombre_pagina \\ "",
+        texto_pagina \\ "",
+        jeraquia_pagina \\ "",
+        id_menu_lateral \\ nil
+      ) do
     pagina = %{
       "nombre" => nombre_pagina,
       "jerarquia" => jeraquia_pagina,
@@ -260,7 +277,6 @@ defmodule Utils do
     id_pagina
   end
 
-
   def url_format(string) do
     string
     |> String.downcase()
@@ -268,7 +284,6 @@ defmodule Utils do
     |> String.replace(~r/[^A-Z^a-z^0-9\s]/u, "")
     |> String.replace(~r/\s/, "-")
   end
-
 
   def crear_navegacion(url_navegacion, nombre_navegacion, id_pagina) do
     vinculo = %{
@@ -294,48 +309,44 @@ defmodule Utils do
     {:ok, id_navegacion} = Map.fetch(response_body_map, "id")
 
     id_navegacion
-
   end
 
   def crear_menu_lateral(nombre_menu) do
-
     menu_lateral = %{
       "nombre" => nombre_menu
     }
 
-    response_menu_laterals = HTTPoison.post!(
-      "https://testing.cms.fiuba.lambdaclass.com/menu-laterals",
-      JSON.encode!(menu_lateral),
-      [{"Content-type", "application/json"}]
-    )
+    response_menu_laterals =
+      HTTPoison.post!(
+        "https://testing.cms.fiuba.lambdaclass.com/menu-laterals",
+        JSON.encode!(menu_lateral),
+        [{"Content-type", "application/json"}]
+      )
 
     response_body = response_menu_laterals.body
     {:ok, response_body_map} = JSON.decode(response_body)
     {:ok, id_menu_lateral} = Map.fetch(response_body_map, "id")
 
     id_menu_lateral
-
   end
 
-
   def actualizar_menu_lateral(id_menu_lateral, id_navegaciones) do
-
-    links = Enum.map(id_navegaciones,
-      fn id_navegacion ->
-        %{"navegacion" => id_navegacion}
-      end
+    links =
+      Enum.map(
+        id_navegaciones,
+        fn id_navegacion ->
+          %{"navegacion" => id_navegacion}
+        end
       )
 
-    menu_lateral = %{ "links"=> links}
+    menu_lateral = %{"links" => links}
 
     HTTPoison.put!(
-      ("https://testing.cms.fiuba.lambdaclass.com/menu-laterals/" <> to_string(id_menu_lateral)),
+      "https://testing.cms.fiuba.lambdaclass.com/menu-laterals/" <> to_string(id_menu_lateral),
       JSON.encode!(menu_lateral),
       [{"Content-type", "application/json"}]
     )
-
   end
-
 
   def cargar_hijos(plid) do
     query_sql = "SELECT
@@ -350,7 +361,6 @@ defmodule Utils do
     respuesta.rows
   end
 
-
   def cargar_nodo(nid) do
     query_sql = "SELECT
   node.title AS titulo_nodo,
@@ -362,7 +372,6 @@ defmodule Utils do
     {:ok, respuesta} = Repo.query(query_sql)
     respuesta.rows
   end
-
 
   def busqueda_recursiva(elemento, url_nav_padre, nombre_nav_padre, jerarquia_padre) do
     nid = elemento |> Enum.at(2)
