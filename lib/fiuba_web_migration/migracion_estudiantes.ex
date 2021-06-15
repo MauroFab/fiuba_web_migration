2f6049f1de8318ce8cc23afdb974a3913293cfa2
noxethiems <davela@fi.uba.ar>
2021-06-15 17:56:56 -0300

    estudiantes tiene menues laterales recursivo

--- lib/fiuba_web_migration/migracion_estudiantes.ex
+++ lib/fiuba_web_migration/migracion_estudiantes.ex
@@ -17,6 +17,7 @@ defmodule Migracion_estudiantes do
   # end
 
   def estudiantes() do
+
     estudiantes = cargar_nodo_padre_standard(1600) |> Enum.at(0)
 
     nid = estudiantes |> Enum.at(2)
@@ -24,19 +25,21 @@ defmodule Migracion_estudiantes do
 
     nombre_pagina = nodo |> Enum.at(0)
     texto_pagina = nodo |> Enum.at(1)
-
-    id_pagina_estudiantes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina)
-
     url_estudiantes = "/estudiantes"
-    crear_navegacion(url_estudiantes, nombre_pagina, id_pagina_estudiantes)
+
+    id_menu_lateral = crear_menu_lateral(url_estudiantes)
+    id_pagina_estudiantes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_estudiantes, nombre_pagina, id_pagina_estudiantes)
 
     estudiantes_opts = estudiantes |> Enum.at(0) |> cargar_hijos()
 
-    Enum.map(
+    ids_navs = Enum.map(
       estudiantes_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_estudiantes, nombre_pagina, nombre_pagina)
+        busqueda_recursiva(elemento, url_estudiantes, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
+
   end
 end
