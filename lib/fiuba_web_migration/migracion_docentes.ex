1c443de281508f2d8a18d122f97d57082e137bba
noxethiems <davela@fi.uba.ar>
2021-06-15 18:01:46 -0300

    docentes con menu lateral recursivo

--- lib/fiuba_web_migration/migracion_docentes.ex
+++ lib/fiuba_web_migration/migracion_docentes.ex
@@ -18,6 +18,7 @@ defmodule Migracion_docentes do
   # end
 
   def docentes() do
+
     docentes = cargar_nodo_padre_standard(906) |> Enum.at(0)
 
     nid = docentes |> Enum.at(2)
@@ -25,19 +26,21 @@ defmodule Migracion_docentes do
 
     nombre_pagina = nodo |> Enum.at(0)
     texto_pagina = nodo |> Enum.at(1)
-
-    id_pagina_docentes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina)
-
     url_docentes = "/docentes"
-    crear_navegacion(url_docentes, nombre_pagina, id_pagina_docentes)
+
+    id_menu_lateral = crear_menu_lateral(url_docentes)
+    id_pagina_docentes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_docentes, nombre_pagina, id_pagina_docentes)
 
     docentes_opts = docentes |> Enum.at(0) |> cargar_hijos()
 
-    Enum.map(
+    ids_navs = Enum.map(
       docentes_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_docentes, nombre_pagina, nombre_pagina)
+        busqueda_recursiva(elemento, url_docentes, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
+
   end
 end
