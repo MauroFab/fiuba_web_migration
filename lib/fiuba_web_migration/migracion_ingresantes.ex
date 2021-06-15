ac2dc8b378ea3973ede3fa98cf1de13361d64f21
noxethiems <davela@fi.uba.ar>
2021-06-15 17:51:18 -0300

    ingresantes recursivo con menues laterales

--- lib/fiuba_web_migration/migracion_ingresantes.ex
+++ lib/fiuba_web_migration/migracion_ingresantes.ex
@@ -48,6 +48,7 @@ defmodule Migracion_ingresantes do
   # end
 
   def ingresantes() do
+
     ingresantes = cargar_nodo_padre_standard(1599) |> Enum.at(0)
 
     nid = ingresantes |> Enum.at(2)
@@ -55,19 +56,20 @@ defmodule Migracion_ingresantes do
 
     nombre_pagina = nodo |> Enum.at(0)
     texto_pagina = nodo |> Enum.at(1)
-
-    id_pagina_ingresantes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina)
-
     url_ingresantes = "/ingresantes"
-    crear_navegacion(url_ingresantes, nombre_pagina, id_pagina_ingresantes)
+
+    id_menu_lateral = crear_menu_lateral(url_ingresantes)
+    id_pagina_ingresantes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_ingresantes, nombre_pagina, id_pagina_ingresantes)
 
     ingresantes_opts = ingresantes |> Enum.at(0) |> cargar_hijos()
 
-    Enum.map(
+    ids_navs = Enum.map(
       ingresantes_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_ingresantes, nombre_pagina, nombre_pagina)
+        busqueda_recursiva(elemento, url_ingresantes, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
 end
