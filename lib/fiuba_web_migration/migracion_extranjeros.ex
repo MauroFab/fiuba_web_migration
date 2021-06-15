8b14701cf1498c1fc2f3f9dbdb4b910660d00f41
noxethiems <davela@fi.uba.ar>
2021-06-15 18:11:34 -0300

    ups

--- lib/fiuba_web_migration/migracion_extranjeros.ex
+++ lib/fiuba_web_migration/migracion_extranjeros.ex
@@ -22,7 +22,7 @@ defmodule Migracion_extranjeros do
     ids_navs = Enum.map(
       extranjeros_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_extranjeros, nombre_pagina, nombre_pagina)
+        busqueda_recursiva(elemento, url_extranjeros, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
     actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
