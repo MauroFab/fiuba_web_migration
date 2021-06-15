6e12fbb43acb29a458d49cef555b19df4cad8a60
noxethiems <davela@fi.uba.ar>
2021-06-15 17:44:35 -0300

    biblioteca con menues laterales recursivo

--- lib/fiuba_web_migration/migracion_biblioteca.ex
+++ lib/fiuba_web_migration/migracion_biblioteca.ex
@@ -53,19 +53,20 @@ defmodule Migracion_biblioteca do
 
     nombre_pagina = nodo |> Enum.at(0)
     texto_pagina = nodo |> Enum.at(1)
-
-    id_pagina_biblioteca = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina)
-
     url_biblioteca = "/biblioteca"
-    crear_navegacion(url_biblioteca, nombre_pagina, id_pagina_biblioteca)
+
+    id_menu_lateral = crear_menu_lateral("biblioteca")
+    id_pagina_biblioteca = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_biblioteca, nombre_pagina, id_pagina_biblioteca)
 
     biblioteca_opts = biblioteca |> Enum.at(0) |> cargar_hijos()
 
-    Enum.map(
+    ids_navs = Enum.map(
       biblioteca_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_biblioteca, nombre_pagina, nombre_pagina)
+        busqueda_recursiva(elemento, url_biblioteca, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
 end
