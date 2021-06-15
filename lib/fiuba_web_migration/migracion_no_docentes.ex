204a710372f2752f9de42858748924e62392fe86
noxethiems <davela@fi.uba.ar>
2021-06-15 18:16:02 -0300

    nodocentes con menu lateral recursivo

--- lib/fiuba_web_migration/migracion_no_docentes.ex
+++ lib/fiuba_web_migration/migracion_no_docentes.ex
@@ -17,6 +17,7 @@ defmodule Migracion_no_docentes do
   # end
 
   def no_docentes() do
+
     no_docentes = cargar_nodo_padre_standard(1604) |> Enum.at(0)
 
     nid = no_docentes |> Enum.at(2)
@@ -24,21 +25,21 @@ defmodule Migracion_no_docentes do
 
     nombre_pagina = nodo |> Enum.at(0)
     texto_pagina = nodo |> Enum.at(1)
-
+    url_no_docentes = "/no-docentes"
     jerarquia_padre = "No-docentes"
 
-    id_pagina_no_docentes = crear_pagina(nombre_pagina, texto_pagina, jerarquia_padre)
-
-    url_no_docentes = "/no-docentes"
-    crear_navegacion(url_no_docentes, nombre_pagina, id_pagina_no_docentes)
+    id_menu_lateral = crear_menu_lateral(url_no_docentes)
+    id_pagina_no_docentes = crear_pagina(nombre_pagina, texto_pagina, jerarquia_padre, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_no_docentes, nombre_pagina, id_pagina_no_docentes)
 
     no_docentes_opts = no_docentes |> Enum.at(0) |> cargar_hijos()
 
-    Enum.map(
+    ids_navs = Enum.map(
       no_docentes_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_no_docentes, nombre_pagina, jerarquia_padre)
+        busqueda_recursiva(elemento, url_no_docentes, nombre_pagina, jerarquia_padre, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
 end
