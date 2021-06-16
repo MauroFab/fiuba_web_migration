5ab8aeb499b933474a7dc70b0f864466ba38be7a
noxethiems <davela@fi.uba.ar>
2021-06-16 19:09:46 -0300

    refactor bienestar

--- lib/fiuba_web_migration/migracion_bienestar.ex
+++ lib/fiuba_web_migration/migracion_bienestar.ex
@@ -4,22 +4,23 @@ defmodule Migracion_bienestar do
 
   def bienestar() do
 
-    bienestar = cargar_nodo_padre_standard(1162) |> Enum.at(0)
+    bienestar = cargar_nodo(453)
 
     texto_pagina = ""
     nombre_pagina = "Bienestar"
     url_bienestar = "/bienestar"
+    jerarquia_bienestar = nombre_pagina
 
     id_menu_lateral = crear_menu_lateral(url_bienestar)
-    id_pagina_bienestar = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_pagina_bienestar = crear_pagina(nombre_pagina, texto_pagina, jerarquia_bienestar, id_menu_lateral)
     id_navegacion = crear_navegacion(url_bienestar, nombre_pagina, id_pagina_bienestar)
 
-    bienestares = bienestar |> Enum.at(0) |> cargar_hijos()
+    bienestares = cargar_hijos(1162)
 
     ids_navs = Enum.map(
       bienestares,
       fn elemento ->
-        busqueda_recursiva(elemento, url_bienestar, nombre_pagina, nombre_pagina, id_menu_lateral)
+        busqueda_recursiva(elemento, url_bienestar, nombre_pagina, jerarquia_bienestar, id_menu_lateral)
       end
     )
     actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
