b283f68ac9c2275e97d515c830f40d19ee8f7bab
noxethiems <davela@fi.uba.ar>
2021-06-16 19:18:29 -0300

    refactor docentes

--- lib/fiuba_web_migration/migracion_docentes.ex
+++ lib/fiuba_web_migration/migracion_docentes.ex
@@ -4,25 +4,23 @@ defmodule Migracion_docentes do
 
   def docentes() do
 
-    docentes = cargar_nodo_padre_standard(906) |> Enum.at(0)
+    docentes = cargar_nodo(285) |> Enum.at(0)
 
-    nid = docentes |> Enum.at(2)
-    nodo = cargar_nodo(nid) |> Enum.at(0)
-
-    nombre_pagina = nodo |> Enum.at(0)
-    texto_pagina = nodo |> Enum.at(1)
+    nombre_pagina = docentes |> Enum.at(0)
+    texto_pagina = docentes |> Enum.at(1)
+    jerarquia = nombre_pagina
     url_docentes = "/docentes"
 
     id_menu_lateral = crear_menu_lateral(url_docentes)
-    id_pagina_docentes = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
+    id_pagina_docentes = crear_pagina(nombre_pagina, texto_pagina, jerarquia, id_menu_lateral)
     id_navegacion = crear_navegacion(url_docentes, nombre_pagina, id_pagina_docentes)
 
-    docentes_opts = docentes |> Enum.at(0) |> cargar_hijos()
+    docentes_opts = cargar_hijos(906)
 
     ids_navs = Enum.map(
       docentes_opts,
       fn elemento ->
-        busqueda_recursiva(elemento, url_docentes, nombre_pagina, nombre_pagina, id_menu_lateral)
+        busqueda_recursiva(elemento, url_docentes, nombre_pagina, jerarquia, id_menu_lateral)
       end
     )
     actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
