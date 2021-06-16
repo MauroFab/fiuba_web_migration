6de1f7f09eaa742b5f37b97e115325b1e6ddfd06
noxethiems <davela@fi.uba.ar>
2021-06-16 16:55:01 -0300

    Carreras d especializacion c menues lat recursivo

--- lib/fiuba_web_migration/migracion_carrera_especializacion.ex
+++ lib/fiuba_web_migration/migracion_carrera_especializacion.ex
@@ -1,77 +1,27 @@
 defmodule Migracion_carrera_especializacion do
-  alias FiubaWebMigration.Repo
+
   import Utils
-  import String
-
-  # def cargar_carreras_especializacion do
-  #   query_sql = "SELECT
-  #       menu_links.mlid AS mlid,
-  #       menu_links.link_title AS titulo
-  #     FROM menu_links
-  #     WHERE menu_links.plid = '1158'
-  #     ORDER BY menu_links.link_title ASC"
-
-  #   {:ok, respuesta} = Repo.query(query_sql)
-  #   respuesta.rows
-  # end
-
-  def cargar_nodos_asociados_carreras_especializadas(mlid) do
-    query_sql =
-      "SELECT
-        menu_links.link_title AS titulo_nodo_asociado,
-        REPLACE(menu_links.link_path, 'node/','') AS nid
-      FROM menu_links
-      WHERE menu_links.plid = " <>
-        to_string(mlid) <> " OR menu_links.mlid = " <> to_string(mlid) <> ";"
-
-    {:ok, respuesta} = Repo.query(query_sql)
-    respuesta.rows
-  end
 
   def carreras_especializaciones() do
-    Enum.map(
-      cargar_nodo_padre_no_standard(1158),
-      fn especializacion ->
-        nombre_carrera = Enum.at(especializacion, 1)
 
-        Enum.map(
-          cargar_nodos_asociados_carreras_especializadas(Enum.at(especializacion, 0)),
-          fn nodo ->
-            texto_asociado = Enum.at(cargar_texto_asociado(Enum.at(nodo, 1)), 0)
+    nombre_pagina = "Carreras de Especialización"
+    jerarquia_pagina = nombre_pagina
+    texto_pagina = ""
+    url_carreras_esp = "/ensenanza/posgrado/carreras-de-especializacion"
 
-            nombre_nodo = texto_asociado |> Enum.at(0)
-            texto_nodo = texto_asociado |> Enum.at(1)
+    id_menu_lateral = crear_menu_lateral(url_carreras_esp)
+    id_pagina_carreras_esp = crear_pagina(nombre_pagina, texto_pagina, jerarquia_pagina, id_menu_lateral)
+    id_navegacion = crear_navegacion(url_carreras_esp, nombre_pagina, id_pagina_carreras_esp)
 
-            jerarquia_pagina =
-              if String.contains?(nombre_nodo, nombre_carrera) do
-                "Posgrado/Carreras de Especializacion/" <> nombre_carrera
-              else
-                "Posgrado/Carreras de Especializacion/" <> nombre_carrera <> "/" <> nombre_nodo
-              end
+    carreras_esp = cargar_hijos(1158)
 
-            # Se crea la página
-            id_pagina = crear_pagina(nombre_nodo, texto_nodo, jerarquia_pagina)
-
-            # Se crea la navegación
-            nombre_navegacion =
-              if String.contains?(nombre_nodo, nombre_carrera) do
-                "Carrera de Especializacion: " <> nombre_carrera
-              else
-                "Carrera de Especializacion: " <> nombre_carrera <> " - " <> nombre_nodo
-              end
-
-            url_navegacion =
-              "/ensenanza/posgrado/carreras-de-especializacion/" <>
-                if String.contains?(nombre_nodo, nombre_carrera) do
-                  nombre_carrera |> url_format()
-                else
-                  (nombre_carrera |> url_format()) <> "/" <> (nombre_nodo |> url_format())
-                end
-
-            crear_navegacion(url_navegacion, nombre_navegacion, id_pagina)
-          end
-        )
+    ids_navs = Enum.map(
+      carreras_esp,
+      fn elemento ->
+        busqueda_recursiva(elemento, url_carreras_esp, nombre_pagina, nombre_pagina, id_menu_lateral)
       end
     )
+    actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
+
 end
