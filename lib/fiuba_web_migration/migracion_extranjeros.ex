16d4c5c740c662937bdc404f730ab23fbd1afbfe
noxethiems <davela@fi.uba.ar>
2021-06-16 19:29:36 -0300

    refactor extranjeros

--- lib/fiuba_web_migration/migracion_extranjeros.ex
+++ lib/fiuba_web_migration/migracion_extranjeros.ex
@@ -4,20 +4,17 @@ defmodule Migracion_extranjeros do
 
   def extranjeros() do
 
-    extranjeros = cargar_nodo_padre_standard(1602) |> Enum.at(0)
+    extranjeros = cargar_nodo(747)|> Enum.at(0)
 
-    nid = extranjeros |> Enum.at(2)
-    nodo = cargar_nodo(nid) |> Enum.at(0)
-
-    nombre_pagina = nodo |> Enum.at(0)
-    texto_pagina = nodo |> Enum.at(1)
+    nombre_pagina = extranjeros |> Enum.at(0)
+    texto_pagina = extranjeros |> Enum.at(1)
     url_extranjeros = "/extranjeros"
 
     id_menu_lateral = crear_menu_lateral(url_extranjeros)
     id_pagina_extranjeros = crear_pagina(nombre_pagina, texto_pagina, nombre_pagina, id_menu_lateral)
     id_navegacion = crear_navegacion(url_extranjeros, nombre_pagina, id_pagina_extranjeros)
 
-    extranjeros_opts = extranjeros |> Enum.at(0) |> cargar_hijos()
+    extranjeros_opts = cargar_hijos(1602)
 
     ids_navs = Enum.map(
       extranjeros_opts,
