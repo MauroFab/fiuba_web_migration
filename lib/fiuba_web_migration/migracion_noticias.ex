acf7d26b376bb425442941b0b1792c15704867c5
noxethiems <davela@fi.uba.ar>
2021-06-20 16:51:42 -0300

    Migracion de noticias cuyo status es publicado

--- lib/fiuba_web_migration/migracion_noticias.ex
+++ lib/fiuba_web_migration/migracion_noticias.ex
@@ -14,7 +14,7 @@ defmodule Migracion_noticias do
       FROM node
       INNER JOIN field_data_body ON  node.nid = field_data_body.entity_id
       LEFT JOIN field_data_field_date ON node.nid = field_data_field_date.entity_id
-      WHERE node.type = 'article'
+      WHERE node.type = 'article' AND node.status = 1
       ORDER BY field_data_field_date.field_date_value DESC;"
 
     {:ok, respuesta} = Repo.query(query_sql)
