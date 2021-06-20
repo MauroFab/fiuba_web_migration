6775272c4b1d7395285eee4065e53df9e609cbaf
noxethiems <davela@fi.uba.ar>
2021-06-20 01:12:18 -0300

    Primer intento de lista de navegaciones

--- lib/fiuba_web_migration/migracion_institucional.ex
+++ lib/fiuba_web_migration/migracion_institucional.ex
@@ -1,28 +1,41 @@
 defmodule Migracion_institucional do
 
+  import String
   import Utils
 
   def institucional() do
 
     # REQUIRE BORRAR UN REGISTRO EN MENU_LINKS: menu_links.link_path = 'node/1406'
     # TAMBIEN menu_link_path = 'departamento/32/novedades'
+    nodo_institucional = cargar_nodo("node/514") |> Enum.at(0)
 
-    texto_pagina = ""
-    nombre_pagina = "Institucional"
+    nombre_pagina = nodo_institucional |> Enum.at(0)
+    texto_pagina = nodo_institucional |> Enum.at(1)
+    nodo_type = nodo_institucional |> Enum.at(3)
     url_institucional = "/institucional"
 
     id_menu_lateral = crear_menu_lateral(url_institucional)
-    id_pagina_institucional = crear_pagina(nombre_pagina, texto_pagina, id_menu_lateral)
+    id_pagina_institucional = crear_pagina(nombre_pagina, id_menu_lateral)
     id_navegacion = crear_navegacion(url_institucional, nombre_pagina, id_pagina_institucional)
 
     institucionales = cargar_hijos(1225)
 
-    ids_navs = Enum.map(
-      institucionales,
-      fn elemento ->
-        busqueda_recursiva(elemento, url_institucional, id_menu_lateral)
-      end
-    )
+    ids_navs =
+      Enum.map(
+        institucionales,
+        fn elemento ->
+          busqueda_recursiva(elemento, url_institucional, id_menu_lateral)
+        end
+      )
+
+    ids_navs_pag = if (contains?(nodo_type,"panel")) do
+      ids_navs
+    else
+      []
+    end
+
+    actualizar_pagina(id_pagina_institucional, texto_pagina, ids_navs_pag)
     actualizar_menu_lateral(id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
+
 end
--- lib/fiuba_web_migration/migracion_prensa.ex
+++ lib/fiuba_web_migration/migracion_prensa.ex
@@ -1,20 +1,22 @@
 defmodule Migracion_prensa do
 
+  import String
   import Utils
 
   def prensa() do
 
-    prensa = cargar_nodo("node/877") |> Enum.at(0)
+    nodo = cargar_nodo("node/877") |> Enum.at(0)
 
-    nombre_pagina = prensa |> Enum.at(0)
-    texto_prensa = prensa |> Enum.at(1)
+    nombre_pagina = nodo |> Enum.at(0)
+    texto_prensa = nodo |> Enum.at(1)
     url_prensa = "/prensa"
+    nodo_type = nodo |> Enum.at(3)
 
     # texto_con_links =
     #   texto_prensa |> String.replace("<a href=\"", "\n") |> String.replace(">(+)</a>", "\n\n")
 
     id_menu_lateral = crear_menu_lateral(url_prensa)
-    id_pagina_prensa = crear_pagina(nombre_pagina, texto_prensa, id_menu_lateral)
+    id_pagina_prensa = crear_pagina(nombre_pagina, id_menu_lateral)
     id_navegacion = crear_navegacion(url_prensa, nombre_pagina, id_pagina_prensa)
 
     prensa_anios = cargar_hijos(917)
@@ -25,6 +27,14 @@ defmodule Migracion_prensa do
         busqueda_recursiva(elemento, url_prensa, id_menu_lateral)
       end
     )
+
+    ids_navs_prensa = if (contains?(nodo_type,"panel")) do
+      ids_navs
+    else
+      []
+    end
+
+    actualizar_pagina(id_pagina_prensa, texto_prensa, ids_navs_prensa)
     actualizar_menu_lateral( id_menu_lateral, [id_navegacion] ++ ids_navs)
   end
 
