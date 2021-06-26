333ba07498afba51f5b86ab6455db29f8a35e292
noxethiems <davela@fi.uba.ar>
2021-06-26 17:55:16 -0300

    paneles y menues laterales que se modifican a mano

--- lib/fiuba_web_migration.ex
+++ lib/fiuba_web_migration.ex
@@ -1,7 +1,8 @@
 defmodule FiubaWebMigration do
 
   import Migracion_noticias
-  import Migracion_paneles
+  import Migracion_paneles_y_menues
+  import Migracion_pdfs
   import Utils
 
   def migration() do
@@ -9,16 +10,18 @@ defmodule FiubaWebMigration do
     # portada_noticias = cargar_imagen("https://testing.cms.fiuba.lambdaclass.com/uploads/Imagenes_noticia_LH_2_da25e81fa7.png","portada_noticias.jpg")
     # noticias(portada_noticias)
 
-    portada_paginas = cargar_imagen("https://testing.cms.fiuba.lambdaclass.com/uploads/Imagenes_noticia_institucional_dcdabf29f7.png", "portada_paginas.jpg")
-    nodos_raices = carga_nodos_raices()
+    # portada_paginas = 42 #cargar_imagen("https://testing.cms.fiuba.lambdaclass.com/uploads/Imagenes_noticia_institucional_dcdabf29f7.png", "portada_paginas.jpg")
+    # nodos_raices = carga_nodos_raices()
 
-    Enum.map(
-      nodos_raices,
-      fn nodo_raiz ->
-        procesar_nodo_raiz(nodo_raiz, portada_paginas)
-      end
-    )
+    # Enum.map(
+    #   nodos_raices,
+    #   fn nodo_raiz ->
+    #     procesar_nodo_raiz(nodo_raiz, portada_paginas)
+    #   end
+    # )
 
     paneles()
+    menues_laterales()
+
   end
 end
